41e56eea5ed385e5f84128036175d7ff7a1bc202
ASHMIT <sashmit796@gmail.com>
2026-06-25 12:41:17 +0530

    Update data.py

--- app/data/data.py
+++ app/data/data.py
@@ -4,7 +4,8 @@ import os
 
 # Get the directory where this file is located
 current_dir = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(current_dir, 'creditcard.csv')
+csv_path = os.path.join(current_dir, 'creditcard.csv')#Kindly download the dataset from the link  given in the  documentation  and save it as creditcard.csv inorder to get this running  we are sorry to not upload the dataset directly due to its  large size trained on 30 features and 284k rows  
+
 data=pd.read_csv(csv_path)
 print(data.shape)
 print(data[data['Class']==0].shape)
